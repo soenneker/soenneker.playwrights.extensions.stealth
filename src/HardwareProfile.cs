3ce1ed5ca67e44d78aaca537f8bf2edebd4cd066
Jake Soenneker <jake@soenneker.com>
2025-07-28 13:32:38 -0500

    don't need to seed

--- src/HardwareProfile.cs
+++ src/HardwareProfile.cs
@@ -1,5 +1,4 @@
 ﻿using Soenneker.Utils.Random;
-using System;
 
 namespace Soenneker.Playwrights.Extensions.Stealth;
 
@@ -9,14 +8,13 @@ public sealed record HardwareProfile(int Cores, int MemoryGb, string Platform, i
     {
         // stable random seed per context
         int seed = RandomUtil.Next(int.MinValue, int.MaxValue);
-        var rnd = new Random(seed);
 
         int[] corePool = [4, 6, 8, 12, 16, 24];
         int[] memoryPool = [8, 16, 32];
         (int w, int h)[] screenPool = [(1920, 1080), (2560, 1440), (3840, 2160)];
 
-        return new HardwareProfile(Cores: corePool[rnd.Next(corePool.Length)], MemoryGb: memoryPool[rnd.Next(memoryPool.Length)], Platform: "Win32",
-            ScreenW: screenPool[rnd.Next(screenPool.Length)].w, ScreenH: screenPool[rnd.Next(screenPool.Length)].h, ChromeVersion: "114.0.5735.133",
+        return new HardwareProfile(Cores: corePool[RandomUtil.Next(corePool.Length)], MemoryGb: memoryPool[RandomUtil.Next(memoryPool.Length)], Platform: "Win32",
+            ScreenW: screenPool[RandomUtil.Next(screenPool.Length)].w, ScreenH: screenPool[RandomUtil.Next(screenPool.Length)].h, ChromeVersion: "114.0.5735.133",
             Seed: seed);
     }
 }
